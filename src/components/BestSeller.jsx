b42f3143a9e9c36d4a1d9393e1495cb62ca773fb
Gayathri7841 <boinagayathri1201@gmail.com>
2026-01-19 10:09:14 +0530

    Update BestSeller.jsx

--- src/components/BestSeller.jsx
+++ src/components/BestSeller.jsx
@@ -13,7 +13,7 @@ setBestSeller(bestProduct.slice(0,5))
   return (
     <div className='my-10'>
         <div className='text-center text-3xl py-8'>
-<Title text1={'BEST'} text2={'SELLER'}></Title>
+<Title text1={'sss'} text2={'SELLER'}></Title>
 <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>bvjdfhu</p>
         </div>
 <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
